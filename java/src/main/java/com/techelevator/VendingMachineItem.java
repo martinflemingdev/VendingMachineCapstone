d9e8fb8943924ef58942e7661ede4827a27757a7
Martin Fleming <78125388+martinflemingdev@users.noreply.github.com>
2021-03-21 19:41:22 -0400

    Update VendingMachineItem.java

--- java/src/main/java/com/techelevator/VendingMachineItem.java
+++ java/src/main/java/com/techelevator/VendingMachineItem.java
@@ -5,11 +5,13 @@ public abstract class VendingMachineItem {
 	// private members
 	private String name;
 	private String price;
+	private String quantity;
 	
 	// constructor
 	public VendingMachineItem(String name, String price) {
 		this.name = name;
 		this.price = price;
+		this.quantity = "5";
 	}
 
 	// Getters
@@ -21,12 +23,31 @@ public abstract class VendingMachineItem {
 		return price;
 	}	
 
+	public String getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	
+	public void reduceQuantity() {
+		if (quantity.equalsIgnoreCase("SOLD OUT")) {
+			//nothing happens
+		}
+		else if (!(quantity.equalsIgnoreCase("SOLD OUT"))) {
+			int quantityAsInt = Integer.parseInt(getQuantity());
+			if(quantityAsInt > 1) {
+				quantityAsInt--;
+				String newQuantity = String.valueOf(quantityAsInt);
+				setQuantity(newQuantity);
+			} else if(quantityAsInt == 1) {
+				setQuantity("SOLD OUT");
+			}
+		} 
+	}
+
 	// Abstract Methods
 	public abstract void dispense();
 	
-	public abstract String getQuantity();
-	
-	public abstract void setQuantity(String quantity);
-	
-	public abstract void reduceQuantity();
 } 
